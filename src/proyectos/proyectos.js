0478922f739948f3694ce6c1bb0c23f713847720
Melody Hunt <mariamelodyhunt@hotmail.com>
2022-12-01 13:17:14 -0300

    agrego demo

--- src/proyectos/proyectos.js
+++ src/proyectos/proyectos.js
@@ -21,7 +21,7 @@ const Proyectos = () => {
               rel="noreferrer"
               className="enlace-color"
             >
-              Deploy
+              Demo
             </a>
             <a
               href="https://github.com/melodyhunt/Blog"
@@ -45,7 +45,7 @@ const Proyectos = () => {
               rel="noreferrer"
               className="enlace-color"
             >
-              Deploy
+              Demo
             </a>
             <a
               href="https://github.com/melodyhunt/Rick-and-morty"
@@ -69,7 +69,7 @@ const Proyectos = () => {
               rel="noreferrer"
               className="enlace-color"
             >
-              Deploy
+              Demo
             </a>
             <a
               href="https://github.com/melodyhunt/Gif-Expert"
@@ -93,7 +93,7 @@ const Proyectos = () => {
               rel="noreferrer"
               className="enlace-color"
             >
-              Deploy
+              Demo
             </a>
           </div>
         </section>
@@ -108,7 +108,7 @@ const Proyectos = () => {
               rel="noreferrer"
               className="enlace-color"
             >
-              Deploy
+              Demo
             </a>
           </div>
         </section>
